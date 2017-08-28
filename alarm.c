d185672d072d9a9de128439d3cdd15da9231882e
Steve <steve@dnseft.com>
2017-08-28 14:33:41 -0500

    Added reference to header and moved various things there. Cleaned up some comments. Added a function to return the desired file name for OGG and APLAY, still hardcoded, but could be loaded from a file at a later date. Cleaned up usage and added examples.

--- alarm.c
+++ alarm.c
@@ -3,13 +3,14 @@
  * to STDOUT. The actions are selectable via command line argument, as are the timeout value,
  * the 'increment' and various program arguments (verbose, quiet, debug, etc).
  *
- *
- * Version: 0.99
  * (C) 2015 KB4OID Labs, A division of Kodetroll Heavy Industries
+ * File: alarm.c
+ * Version: 0.99
  * Author: Kodetroll (KB4OID - Steve McCarter)
  * Date Created: April 2015
  * Date Previous: None
  * Date Last: April 8 2015
+ * Date Last: August 28 2017
  *
  * To Build (Compile): type 'make'
  *
@@ -37,28 +38,8 @@
 #include <string.h>
 #include <time.h>
 #include <getopt.h>
-//#include "alarm.c"
+#include "alarm.h"
 
-#define VER_MAJOR 0
-#define VER_MINOR 99
-
-#define LOOP_DELAY 1000000
-
-#define DEFAULT_WAVFILE "./test"
-
-enum Actions {
-	ACT_NONE,
-	ACT_PLAY_WAV,	// aplay wavfile
-	ACT_PLAY_BEEP,	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
-	ACT_PLAY_BEEPS,	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga
-	ACT_PLAY_DRIP,	// ogg123 /usr/share/sounds/gnome/default/alerts/drip.ogg
-	ACT_PLAY_SONAR,	// ogg123 /usr/share/sounds/gnome/default/alerts/sonar.ogg
-	ACT_PLAY_GLASS,	// ogg123 /usr/share/sounds/gnome/default/alerts/glass.ogg
-	ACT_PLAY_BELL,	// ogg123 /usr/share/sounds/ubuntu/stereo/bell.ogg
-	ACT_PLAY_POP,	// ogg123 /usr/share/sounds/ubuntu/stereo/message.ogg
-	ACT_PLAY_ERRR,	// ogg123 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
-	ACT_PLAY_RING,	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga
-};
 
 static int verbose;
 static int debug;
@@ -68,20 +49,16 @@ static int now;
 
 char* wavfile;
 char* appname;
+char* player = DEFAULT_PLAYER;
+char tmpbuf[120];	// for getActionFile return value
 
-void cpright(void);
-void usage(void);
-void version(void);
-void Action(int action);
-int getActionTypeFromName(char* atype);
-int getActionTypeFromLabel(char* atype);
-char* getActionTypeName(int action);
-
+/* print the copyright info */
 void cpright(void)
 {
 	printf("%s (C) 2015-2017 KB4OID Labs, A division of Kodetroll Heavy Industries\n",appname);
 }
 
+/* print the copyright and usage(help) info */
 void usage(void)
 {
 	cpright();
@@ -100,15 +77,74 @@ void usage(void)
 	printf("  --time, -t <TIME>      Sets timeout time (in Secs). \n");
 	printf("  --wav, -w <WAV>        Sets alarm wavfile name. \n");
 	printf("\n");
+	printf("Examples: \n");
+	printf("   alarm --now -a PLAY_GLASS - Plays action 'PLAY_GLASS' right now.\n");
+	printf("   alarm --actions - Show list of possible actions.\n");
+	printf("   alarm --action PLAY_BEEP --time 120 - Play beep in about 120 seconds.\n");
+	printf("\n");
 
 }
 
+/* print the copyright and version info */
 void version(void)
 {
 	cpright();
 	printf("Version %d.%d\n",VER_MAJOR,VER_MINOR);
 }
 
+/* return the ogg file for the specified actions, added to make loading dynamically easier in the future */
+/* Eventually, this function will scan through a text file and load the filename associated
+ * with the action and return that. For now, we will use the previously hardcoded examples.
+ * These filenames should include the full path to the sound samples.
+ */
+char* getActionFile(int action)
+{
+	if (verbose)
+		printf("Action: '%s'\n",getActionTypeName(action));
+
+	switch(action)
+	{
+		case ACT_PLAY_WAV :	// wavfile
+			return(wavfile);
+			break;
+		case ACT_PLAY_BEEP :	// /usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
+			return("/usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga");
+			break;
+		case ACT_PLAY_BEEPS :	// /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga
+			return("/usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga");
+			break;
+		case ACT_PLAY_DRIP :	// /usr/share/sounds/gnome/default/alerts/drip.ogg
+			//return("/usr/share/sounds/gnome/default/alerts/drip.ogg");
+			return("/usr/share/sounds/misc/drip.ogg");	// location of some sounds changed between versions
+			break;
+		case ACT_PLAY_SONAR :	// /usr/share/sounds/gnome/default/alerts/sonar.ogg
+			//return("/usr/share/sounds/gnome/default/alerts/sonar.ogg");
+			return("/usr/share/sounds/misc/sonar.ogg");	// location of some sounds changed between versions
+			break;
+		case ACT_PLAY_GLASS :	// /usr/share/sounds/gnome/default/alerts/glass.ogg
+			//return("/usr/share/sounds/gnome/default/alerts/glass.ogg");
+			return("/usr/share/sounds/misc/glass.ogg");	// location of some sounds changed between versions
+			break;
+		case ACT_PLAY_BELL :	// /usr/share/sounds/ubuntu/stereo/bell.ogg
+			return("/usr/share/sounds/ubuntu/stereo/bell.ogg");
+			break;
+		case ACT_PLAY_POP :	// /usr/share/sounds/ubuntu/stereo/message.ogg
+			return("/usr/share/sounds/ubuntu/stereo/message.ogg");
+			break;
+		case ACT_PLAY_ERRR :	// /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+			return("/usr/share/sounds/freedesktop/stereo/suspend-error.oga");
+			break;
+		case ACT_PLAY_RING :	// /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga
+			return("/usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga");
+			break;
+		case ACT_NONE :
+		default :
+			return("");
+			break;
+	}
+}
+
+/* take the specified action */
 void Action(int action)
 {
 	char tbuf[120];
@@ -118,44 +154,54 @@ void Action(int action)
 
 	switch(action)
 	{
-		case ACT_PLAY_WAV :	// aplay wavfile
-			sprintf(tbuf,"aplay %s",wavfile);
+		case ACT_PLAY_WAV :	// play wavfile
+			sprintf(tbuf,"aplay %s",getActionFile(action));
 			system(tbuf);
 			break;
-		case ACT_PLAY_BEEP :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
-			system("ogg123 -q /usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga");
+		case ACT_PLAY_BEEP :	// Play BEEP
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_BEEPS :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga
-			system("ogg123 -q /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga");
+		case ACT_PLAY_BEEPS :	// Play BEEPs
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_DRIP :	// ogg123 /usr/share/sounds/gnome/default/alerts/drip.ogg
-			system("ogg123 -q /usr/share/sounds/gnome/default/alerts/drip.ogg");
+		case ACT_PLAY_DRIP :	// Play Drip
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_SONAR :	// ogg123 /usr/share/sounds/gnome/default/alerts/sonar.ogg
-			system("ogg123 -q /usr/share/sounds/gnome/default/alerts/sonar.ogg");
+		case ACT_PLAY_SONAR :	// Play Sonar
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_GLASS :	// ogg123 /usr/share/sounds/gnome/default/alerts/glass.ogg
-			system("ogg123 -q /usr/share/sounds/gnome/default/alerts/glass.ogg");
+		case ACT_PLAY_GLASS :	// Play Glass
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_BELL :	// ogg123 /usr/share/sounds/ubuntu/stereo/bell.ogg
-			system("ogg123 -q /usr/share/sounds/ubuntu/stereo/bell.ogg");
+		case ACT_PLAY_BELL :	// Play Bell
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_POP :	// ogg123 /usr/share/sounds/ubuntu/stereo/message.ogg
-			system("ogg123 -q /usr/share/sounds/ubuntu/stereo/message.ogg");
+		case ACT_PLAY_POP :	// Play Pop
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_ERRR :	// ogg123 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
-			system("ogg123 -q /usr/share/sounds/freedesktop/stereo/suspend-error.oga ");
+		case ACT_PLAY_ERRR :	// Play Errr
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_PLAY_RING :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga
-			system("ogg123 -q /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga	");
+		case ACT_PLAY_RING :	// Play Ring
+			sprintf(tbuf,"%s %s",player,getActionFile(action));
+			system(tbuf);
 			break;
-		case ACT_NONE :
+		case ACT_NONE :	// Do nothing
 		default :
 			printf("No ACTION Selected!\n");
 			break;
 	}
 }
 
+/* get action type from name, returns the enumerated int of the action specified by the char string name */
 int getActionTypeFromName(char* atype)
 {
 	if (strcmp(atype,"NONE") == 0)
@@ -184,6 +230,7 @@ int getActionTypeFromName(char* atype)
 		return(ACT_NONE);
 }
 
+/* get action type from label, returns the enumerated int of the action specified by the char string label */
 int getActionTypeFromLabel(char* atype)
 {
 	if (strcmp(atype,"ACT_NONE") == 0)
@@ -212,38 +259,39 @@ int getActionTypeFromLabel(char* atype)
 		return(ACT_NONE);
 }
 
+/* get action type name, returns the char string describing the action specified by the provided int */
 char* getActionTypeName(int action)
 {
 	switch(action)
 	{
-		case ACT_PLAY_WAV :	// aplay wavfile
+		case ACT_PLAY_WAV :
 			return("ACT_PLAY_WAV");
 			break;
-		case ACT_PLAY_BEEP :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
+		case ACT_PLAY_BEEP :
 			return("ACT_PLAY_BEEP");
 			break;
-		case ACT_PLAY_BEEPS :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga
+		case ACT_PLAY_BEEPS :
 			return("ACT_PLAY_BEEPS");
 			break;
-		case ACT_PLAY_DRIP :	// ogg123 /usr/share/sounds/gnome/default/alerts/drip.ogg
+		case ACT_PLAY_DRIP :
 			return("ACT_PLAY_DRIP");
 			break;
-		case ACT_PLAY_SONAR :	// ogg123 /usr/share/sounds/gnome/default/alerts/sonar.ogg
+		case ACT_PLAY_SONAR :
 			return("ACT_PLAY_SONAR");
 			break;
-		case ACT_PLAY_GLASS :	// ogg123 /usr/share/sounds/gnome/default/alerts/glass.ogg
+		case ACT_PLAY_GLASS :
 			return("ACT_PLAY_GLASS");
 			break;
-		case ACT_PLAY_BELL :	// ogg123 /usr/share/sounds/ubuntu/stereo/bell.ogg
+		case ACT_PLAY_BELL :
 			return("ACT_PLAY_BELL");
 			break;
-		case ACT_PLAY_POP :	// ogg123 /usr/share/sounds/ubuntu/stereo/message.ogg
+		case ACT_PLAY_POP :
 			return("ACT_PLAY_POP");
 			break;
-		case ACT_PLAY_ERRR :	// ogg123 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+		case ACT_PLAY_ERRR :
 			return("ACT_PLAY_ERRR");
 			break;
-		case ACT_PLAY_RING :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga
+		case ACT_PLAY_RING :
 			return("ACT_PLAY_RING");
 			break;
 		case ACT_NONE :
@@ -256,38 +304,39 @@ char* getActionTypeName(int action)
 
 }
 
+/* get action name, returns the char string name of the action specified by the provided int */
 char* getActionName(int action)
 {
 	switch(action)
 	{
-		case ACT_PLAY_WAV :	// aplay wavfile
+		case ACT_PLAY_WAV :
 			return("PLAY_WAV");
 			break;
-		case ACT_PLAY_BEEP :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
+		case ACT_PLAY_BEEP :
 			return("PLAY_BEEP");
 			break;
-		case ACT_PLAY_BEEPS :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga
+		case ACT_PLAY_BEEPS :
 			return("PLAY_BEEPS");
 			break;
-		case ACT_PLAY_DRIP :	// ogg123 /usr/share/sounds/gnome/default/alerts/drip.ogg
+		case ACT_PLAY_DRIP :
 			return("PLAY_DRIP");
 			break;
-		case ACT_PLAY_SONAR :	// ogg123 /usr/share/sounds/gnome/default/alerts/sonar.ogg
+		case ACT_PLAY_SONAR :
 			return("PLAY_SONAR");
 			break;
-		case ACT_PLAY_GLASS :	// ogg123 /usr/share/sounds/gnome/default/alerts/glass.ogg
+		case ACT_PLAY_GLASS :
 			return("PLAY_GLASS");
 			break;
-		case ACT_PLAY_BELL :	// ogg123 /usr/share/sounds/ubuntu/stereo/bell.ogg
+		case ACT_PLAY_BELL :
 			return("PLAY_BELL");
 			break;
-		case ACT_PLAY_POP :	// ogg123 /usr/share/sounds/ubuntu/stereo/message.ogg
+		case ACT_PLAY_POP :
 			return("PLAY_POP");
 			break;
-		case ACT_PLAY_ERRR :	// ogg123 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+		case ACT_PLAY_ERRR :
 			return("PLAY_ERRR");
 			break;
-		case ACT_PLAY_RING :	// ogg123 /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga
+		case ACT_PLAY_RING :
 			return("PLAY_RING");
 			break;
 		case ACT_NONE :
@@ -303,7 +352,7 @@ char* getActionName(int action)
 int main(int argc, char* argv[])
 {
 	long timeout = 5;
-	long timer = time(NULL);
+	long timer = time(NULL);	// sets timer to current time
 	int c;
 	int i;
 
@@ -455,6 +504,7 @@ int main(int argc, char* argv[])
 		putchar ('\n');
 	}
 
+	// add timeout value to current time
 	timer += timeout;
 	if (debug)
 		printf("Timer set for: %ld Secs\n",timer);
